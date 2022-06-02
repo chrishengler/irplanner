4587cc87befc108fdbfd9f039bcf6973121cdc65
Christopher Hengler <dr.c.hengler@gmail.com>
2022-06-02 21:58:27 +0200

    mostly some template stuff, also some view and migration updates

--- planner/models.py
+++ planner/models.py
@@ -1,8 +1,11 @@
+from datetime import datetime, time
 from django.db import models
 
 
 class Series(models.Model):
     series_name = models.CharField(max_length=200)
+    start_time = models.TimeField()
+    time_between_sessions = models.DurationField()
 
     def __str__(self):
         return self.series_name
@@ -24,13 +27,38 @@ class Week(models.Model):
     def __str__(self):
         return f"{self.series.series_name} week {self.week_no}"
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        timeslots = list[time]()
+        start_time = self.series.start_time
+        timeslots.append(start_time)
+        slot_datetime = datetime(2000, 1, 1, hour=start_time.hour, minute=start_time.minute)
+        slot_datetime += self.series.time_between_sessions
+        while slot_datetime < datetime(2000, 1, 2):
+            timeslots.append(slot_datetime.time())
+            slot_datetime += self.series.time_between_sessions
+        Session.objects.filter(week=self).delete()
+        for day in range(0, 7):
+            for slot in timeslots:
+                Session.objects.create(week=self, day=day, time=slot)
+
 
 class Session(models.Model):
     week = models.ForeignKey(Week, on_delete=models.CASCADE)
+    day = models.IntegerField()
     time = models.TimeField()
 
     def __str__(self):
-        return f"{self.week.__str__()} at {self.time}"
+        return f"{self.week.__str__()}: day {self.day} at {self.time}"
+
+    def day_name(self):
+        return {0: "Tuesday",
+                1: "Wednesday",
+                2: "Thursday",
+                3: "Friday",
+                4: "Saturday",
+                5: "Sunday",
+                6: "Monday"}[self.day]
 
 
 class Racer(models.Model):
--- planner/views.py
+++ planner/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.template import loader
 
-from .models import Series, Week
+from .models import Series, Session, Week
 
 
 def index(request):
@@ -25,8 +25,13 @@ def series(request, series_id):
 
 def week(request, week_id):
     w = get_object_or_404(Week, pk=week_id)
-    session_list = w.session_set.all()
+    session_list = w.session_set.all().order_by('time')
+    session_matrix = list[list[Session]]()
+    for day in range(0, 7):
+        session_matrix.append(list[Session]())
+    for session in session_list:
+        session_matrix[session.day].append(session)
     context = {
-        'session_list': session_list
+        'session_matrix': session_matrix
     }
     return render(request, 'planner/week.html', context)
